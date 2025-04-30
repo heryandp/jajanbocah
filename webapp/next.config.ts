818947674ae55ab499182814a77985eef21883b4
Heryan <haxornesia@gmail.com>
2025-04-30 08:17:56 +0700

    Refactor Next.js configuration to use CommonJS module syntax for PWA integration. Simplify PWA settings and enable strict mode for React.

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,13 +1,11 @@
-import type { NextConfig } from "next";
-import withPWA from 'next-pwa';
-
-const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 });
 
-export default nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+  // konfigurasi Next.js lain jika ada
+});
